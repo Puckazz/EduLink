711a62a8403c9a8bd733af1ed7238ea877c799c1
Bao Phuc Dev <ppbao1912@gmail.com>
2026-02-09 11:16:32 +0700

    Add important API modules

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,12 +2,30 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
+import { AuthModule } from './auth/auth.module';
+import { AdminModule } from './admin/admin.module';
+import { ParentModule } from './parent/parent.module';
+import { StudentModule } from './student/student.module';
+import { SubjectModule } from './subject/subject.module';
+import { ScoreModule } from './score/score.module';
+import { AttendanceModule } from './attendance/attendance.module';
+import { NotificationModule } from './notification/notification.module';
+import { FeedbackModule } from './feedback/feedback.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
     }),
+    AuthModule,
+    AdminModule,
+    ParentModule,
+    StudentModule,
+    SubjectModule,
+    ScoreModule,
+    AttendanceModule,
+    NotificationModule,
+    FeedbackModule,
   ],
   controllers: [AppController],
   providers: [AppService],
